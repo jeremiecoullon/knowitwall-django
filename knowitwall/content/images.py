d23f0afadf4db9c3809efc77c6f3b3e021fd4ea9
jeremie's mac <jeremie.coullon.12@ucl.ac.uk>
2018-01-03 18:48:24 +0000

    transcript image parsing works OK. TODO: be able to change between left and middle. And add 'double' option

--- knowitwall/content/images.py
+++ knowitwall/content/images.py
@@ -1,8 +1,40 @@
 import bs4 as bs
 
-def wrap(to_wrap, wrap_in):
+def _wrap(to_wrap, wrap_in):
     contents = to_wrap.replace_with(wrap_in)
     wrap_in.append(contents)
+
+
+def wrap_img(bs_transcript, img_tag, image_class, figure_class):
+    """
+    Wrap <img> tag based on class info.
+
+    Parameters
+    ----------
+    bs_transcript: BeautifulSoup (bs4) object
+    img_tag: bs4 image tag
+    image_class, figure_class: str
+        classes for the img and figure tags
+    """
+    img_src = img_tag.attrs['src']
+
+    if img_tag.has_attr("title"):
+        img_desc = img_tag.attrs['title']
+    else:
+        img_desc = ""
+
+    img_tag.attrs['class'] = image_class
+    img_tag.attrs['style'] = ""
+    new_figure_tag = bs_transcript.new_tag("figure", **{'class': figure_class})
+    new_anchor_tag = bs_transcript.new_tag("a", **{'class':'fancybox_transcript', 'href': img_src,
+                                                   'title': img_desc})
+
+    new_figcaption_tag = bs_transcript.new_tag("figcaption")
+    new_figcaption_tag.string = img_desc
+    _wrap(img_tag, new_figure_tag)
+    img_tag.insert_after(new_figcaption_tag)
+    _wrap(img_tag, new_anchor_tag)
+    return bs_transcript
 
 def create_transcript_image(input_html):
     """
@@ -22,35 +54,38 @@ def create_transcript_image(input_html):
 
     for img_tag in bs_transcript.findAll("img"):
 
-        if img_tag.has_attr("class"):
-            image_type = img_tag['class'][0]
+        if img_tag.has_attr("longdesc"):
+            image_type = img_tag['longdesc']
         else:
-            continue
+            image_type = 'not_set'
 
-        if image_type=='middle':
+        if image_type == 'middle':
             image_class = "tr_image_middle"
             figure_class = "figure_transcript_middle"
         elif image_type =="left":
             image_class = "tr_image_left"
             figure_class = "figure_transcript_left"
+        elif image_type == 'not_set':
+            image_class = "tr_image_middle"
+            figure_class = "figure_transcript_middle"
+
+        if img_tag.parent.name != 'a':
+            bs_transcript = wrap_img(bs_transcript=bs_transcript, img_tag=img_tag,
+                                     image_class=image_class, figure_class=figure_class)
+        elif img_tag.parent.name == 'a':
+            # 1. Refresh <a> link with ficaption
+            # If figcaption was removed, add a new one
+            figcaption_tag = img_tag.parent.parent.find("figcaption")
+            if figcaption_tag is not None:
+                new_img_desc = "".join((str(elem) for elem in figcaption_tag.contents))
+            else:
+                new_figcaption_tag = bs_transcript.new_tag("figcaption")
+                img_tag.parent.insert_after(new_figcaption_tag)
+                new_img_desc = ""
+            img_tag.parent['title'] = new_img_desc
+
+            # 2. check class and refresh based on longdescr
+
         else:
-            continue
-
-        img_src = img_tag.attrs['src']
-        if img_tag.has_attr("longdesc"):
-            img_desc = img_tag.attrs['longdesc']
-        else:
-            img_desc = ""
-
-        img_tag.attrs['class']=image_class
-        img_tag.attrs['style']=""
-        new_figure_tag = bs_transcript.new_tag("figure", **{'class': figure_class})
-        new_anchor_tag = bs_transcript.new_tag("a", **{'class':'fancybox_transcript', 'href': img_src,
-                                                       'title': img_desc})
-
-        new_figcaption_tag = bs_transcript.new_tag("figcaption")
-        new_figcaption_tag.string = img_desc
-        wrap(img_tag, new_figure_tag)
-        img_tag.insert_after(new_figcaption_tag)
-        wrap(img_tag, new_anchor_tag)
+            pass
     return str(bs_transcript)
