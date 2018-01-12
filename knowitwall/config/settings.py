56b36c7d434b8add94813932b4494682a68120eb
jeremie's mac <jeremie.coullon.12@ucl.ac.uk>
2018-01-12 09:04:59 +0000

    added www.knowitwall.com to ALLOWED_HOSTS (forgot to add the www.* version...)

--- knowitwall/config/settings.py
+++ knowitwall/config/settings.py
@@ -14,7 +14,7 @@ else:
     DEBUG = True
 
 
-ALLOWED_HOSTS = ["165.227.237.83", "localhost", "knowitwall.com"]
+ALLOWED_HOSTS = ["165.227.237.83", "localhost", "knowitwall.com", "www.knowitwall.com"]
 
 
 INSTALLED_APPS = [
